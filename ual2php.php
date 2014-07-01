e2b7ca646035f38c68eae3d2fe82fefdc9c96070
uelei <github@uelei.com>
2014-07-01 12:07:14 -0300

    addicionado cod Real

--- ual2php.php
+++ ual2php.php
@@ -7,14 +7,15 @@ $file = file_get_contents($argv[1]);
 
 //remover prog e fim
 //$remover = array('prog','fim'); 
-$file = str_replace("prog ", " <?php ///",$file);
+$file = str_replace("prog ", " <?php //",$file);
 $file = str_replace("fimprog", " ?>",$file);
 $file = str_replace("imprima", "echo",$file);
+$file = str_replace("para", "for", $file);
 $file = str_replace("<-", "=",$file);
 
 
 //encontrando primeira declaracao de int 
-$posicaoIntInicial = strpos($file,"int ");
+$posicaoIntInicial = strpos($file,"int");
 //encontrando o fim da linha 
 $posicaoIntFinal = strpos($file,";",$posicaoIntInicial);
 //pegando as variaveis 
@@ -36,6 +37,28 @@ $file = substr_replace($file,$linhaStringvariaveisInt,$posicaoIntInicial,0);
 
 
 
+//encontrando primeira declaracao de real  
+$posicaoRealInicial = strpos($file,"real");
+//encontrando o fim da linha 
+$posicaoRealFinal = strpos($file,";",$posicaoRealInicial);
+//pegando as variaveis 
+$variaveisRealS = substr($file,$posicaoRealInicial,$posicaoRealFinal-$posicaoRealInicial);
+//$variaveisInt = ;
+//declarando as variaveis int 
+$variaveisReal = explode(',',str_replace(array('float ',';',' '),"",$variaveisRealS));
+//limpa a linha variaveis int 
+$iReal=0;
+$linhaStringvariaveisReal = "";
+$file = substr_replace($file," ",$posicaoRealInicial,$posicaoRealFinal-$posicaoRealInicial+1);
+foreach ($variaveisReal as $varReal) {
+    $s ="(float)$".$varReal;	
+	$file = str_replace($varReal,$s,$file);
+	$iReal++;
+	$linhaStringvariaveisReal = $linhaStringvariaveisReal.$s."=0.0;\n ";
+}
+$file = substr_replace($file,$linhaStringvariaveisReal,$posicaoRealInicial,0);
+
+
 
 
 
@@ -49,7 +72,6 @@ $lll= $ll - $l;
 $idsas = substr($file,$l+5,$lll);
 $st ='$handle = fopen ("php://stdin","r");'.explode(";", $idsas)[0].' = fgets($handle)';
 $file = substr_replace($file, $st ,$l,$lll);
-
 
 //substituindo operacao de div 
 $file = str_replace("div", "/",$file);
